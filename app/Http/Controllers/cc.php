56cc023b9c5dad6714ebf70c51f25c1de9841507
Mohamad.mu <mero22.4.1997>
2020-10-03 16:54:31 +0300

    m

--- app/Http/Controllers/cc.php
+++ app/Http/Controllers/cc.php
@@ -12,7 +12,7 @@ class cc extends Controller
     function recive(Request $request){
 
         $telegram = new Api('939919494:AAHHzgqUYKZ5STaV6nI0kFjhkO4mJw2ZvjU');
-        $update = $telegram->getUpdates();
+       // $update = $telegram->getUpdates();
         $response = $telegram->sendMessage([
             'chat_id' => '190861649',
             'text' => 'Hello ',
