d1ae25d7af7cbda814ae56f81d9462a1e41a39ed
Mohammed Murad <m.murad@ucg.ae>
2021-06-20 16:55:48 +0300

    m

--- app/Http/Controllers/cc.php
+++ app/Http/Controllers/cc.php
@@ -23,7 +23,7 @@ class cc extends Controller
 
             'method' => 'GET',
 
-            'header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.47 Safari/537.36',
+            'header' => 'User-Agent: PostmanRuntime/7.28.0',
 
         ],
 
